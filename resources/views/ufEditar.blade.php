6f1c69af2deb8529acca5560130f917e72ce00be
Thiago <thiagogaleno1301@hotmail.com>
2019-08-26 18:22:44 -0300

    bugfix - Corrigido bug em que nao salvava o novo UF ao editar

--- resources/views/ufEditar.blade.php
+++ resources/views/ufEditar.blade.php
@@ -1,28 +1,28 @@
 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'AC' ? 'selected="selected"' : '' }}>Acre</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'AL' ? 'selected="selected"' : '' }}>Alagoas</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'AP' ? 'selected="selected"' : '' }}>Amapá</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'AM' ? 'selected="selected"' : '' }}>Amazonas</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'BA' ? 'selected="selected"' : '' }}>Bahia</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'CE' ? 'selected="selected"' : '' }}>Ceará</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'DF' ? 'selected="selected"' : '' }}>Distrito Federal</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'ES' ? 'selected="selected"' : '' }}>Espírito Santo</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'GO' ? 'selected="selected"' : '' }}>Goiás</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'MA' ? 'selected="selected"' : '' }}>Maranhão</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'MT' ? 'selected="selected"' : '' }}>Mato Grosso</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'MS' ? 'selected="selected"' : '' }}>Mato Grosso do Sul</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'MG' ? 'selected="selected"' : '' }}>Minas Gerais</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'PA' ? 'selected="selected"' : '' }}>Pará</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'PB' ? 'selected="selected"' : '' }}>Paraíba</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'PR' ? 'selected="selected"' : '' }}>Paraná</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'PE' ? 'selected="selected"' : '' }}>Pernambuco</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'PI' ? 'selected="selected"' : '' }}>Piauí</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'RJ' ? 'selected="selected"' : '' }}>Rio de Janeiro</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'RN' ? 'selected="selected"' : '' }}>Rio Grande do Norte</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'RS' ? 'selected="selected"' : '' }}>Rio Grande do Sul</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'RO' ? 'selected="selected"' : '' }}>Rondônia</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'RR' ? 'selected="selected"' : '' }}>Roraima</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'SC' ? 'selected="selected"' : '' }}>Santa Catarina</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'SP' ? 'selected="selected"' : '' }}>São Paulo</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'SE' ? 'selected="selected"' : '' }}>Sergipe</option> 
-<option value="{{ $dados->uf }}" {{ $dados->uf == 'TO' ? 'selected="selected"' : '' }}>Tocantins</option> 
+<option value="AC" {{ $dados->uf == 'AC' ? 'selected="selected"' : '' }}>Acre</option> 
+<option value="AL" {{ $dados->uf == 'AL' ? 'selected="selected"' : '' }}>Alagoas</option> 
+<option value="AP" {{ $dados->uf == 'AP' ? 'selected="selected"' : '' }}>Amapá</option> 
+<option value="AM" {{ $dados->uf == 'AM' ? 'selected="selected"' : '' }}>Amazonas</option> 
+<option value="BA" {{ $dados->uf == 'BA' ? 'selected="selected"' : '' }}>Bahia</option> 
+<option value="CE" {{ $dados->uf == 'CE' ? 'selected="selected"' : '' }}>Ceará</option> 
+<option value="DF" {{ $dados->uf == 'DF' ? 'selected="selected"' : '' }}>Distrito Federal</option> 
+<option value="ES" {{ $dados->uf == 'ES' ? 'selected="selected"' : '' }}>Espírito Santo</option> 
+<option value="GO" {{ $dados->uf == 'GO' ? 'selected="selected"' : '' }}>Goiás</option> 
+<option value="MA" {{ $dados->uf == 'MA' ? 'selected="selected"' : '' }}>Maranhão</option> 
+<option value="MT" {{ $dados->uf == 'MT' ? 'selected="selected"' : '' }}>Mato Grosso</option> 
+<option value="MS" {{ $dados->uf == 'MS' ? 'selected="selected"' : '' }}>Mato Grosso do Sul</option> 
+<option value="MG" {{ $dados->uf == 'MG' ? 'selected="selected"' : '' }}>Minas Gerais</option> 
+<option value="PA" {{ $dados->uf == 'PA' ? 'selected="selected"' : '' }}>Pará</option> 
+<option value="PB" {{ $dados->uf == 'PB' ? 'selected="selected"' : '' }}>Paraíba</option> 
+<option value="PR" {{ $dados->uf == 'PR' ? 'selected="selected"' : '' }}>Paraná</option> 
+<option value="PE" {{ $dados->uf == 'PE' ? 'selected="selected"' : '' }}>Pernambuco</option> 
+<option value="PI" {{ $dados->uf == 'PI' ? 'selected="selected"' : '' }}>Piauí</option> 
+<option value="RJ" {{ $dados->uf == 'RJ' ? 'selected="selected"' : '' }}>Rio de Janeiro</option> 
+<option value="RN" {{ $dados->uf == 'RN' ? 'selected="selected"' : '' }}>Rio Grande do Norte</option> 
+<option value="RS" {{ $dados->uf == 'RS' ? 'selected="selected"' : '' }}>Rio Grande do Sul</option> 
+<option value="RO" {{ $dados->uf == 'RO' ? 'selected="selected"' : '' }}>Rondônia</option> 
+<option value="RR" {{ $dados->uf == 'RR' ? 'selected="selected"' : '' }}>Roraima</option> 
+<option value="SC" {{ $dados->uf == 'SC' ? 'selected="selected"' : '' }}>Santa Catarina</option> 
+<option value="SP" {{ $dados->uf == 'SP' ? 'selected="selected"' : '' }}>São Paulo</option> 
+<option value="SE" {{ $dados->uf == 'SE' ? 'selected="selected"' : '' }}>Sergipe</option> 
+<option value="TO" {{ $dados->uf == 'TO' ? 'selected="selected"' : '' }}>Tocantins</option> 
